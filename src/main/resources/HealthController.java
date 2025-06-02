12f562de4399c5d3ecf1c7514367cc07efdc3a17
salman nawaz <123812578+Johny111ishxb@users.noreply.github.com>
2025-06-02 22:12:26 +0500

    Create HealthController.java

---
+++ src/main/resources/HealthController.java
@@ -0,0 +1,12 @@
+package ocp.maintenance.prediction.controller;
+
+     import org.springframework.web.bind.annotation.GetMapping;
+     import org.springframework.web.bind.annotation.RestController;
+
+     @RestController
+     public class HealthController {
+         @GetMapping("/health")
+         public String healthCheck() {
+             return "Application is running!";
+         }
+     }
