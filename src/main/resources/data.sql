5ae8d5d887480b55c4ea51c872ac9d8dda8f6de2
salman nawaz <123812578+Johny111ishxb@users.noreply.github.com>
2025-06-02 21:45:20 +0500

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, password, enabled) VALUES ('admin', '$2a$10$3jJ8e1Z5Qz7z6l2X9oZ7aO0z9cZ5Qz7z6l2X9oZ7aO0z9cZ5Qz7z', true);
+     INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_ADMINISTRATEUR');
+     INSERT INTO equipment (name, description) VALUES ('Test Equipment', 'Sample equipment for testing');
